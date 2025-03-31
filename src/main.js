0a48b995eb22672368931e3eb6cfd2d1390e7447
Rodrigo <rstuteu@gmail.com>
2025-03-30 21:48:01 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(nome) {
+    return "Ol\u00E1 ".concat(nome);
+}
+console.log(multiplicar(3, 4)); // Saída: 12
+console.log(saudar("João")); // Saída: Olá João
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+
+function saudar(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+
+console.log(multiplicar(3, 4)); // Saída: 12
+console.log(saudar("João"));    // Saída: Olá João
